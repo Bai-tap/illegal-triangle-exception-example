821fa9e4d7aeef63834d9a2b4b0652a734adfb76
longhoang94 <longsutls@gmail.com>
2018-09-26 20:20:02 +0700

    add `TriangleException`

--- src/IlligalTriangleException.java
+++ src/IlligalTriangleException.java
@@ -1,33 +1,5 @@
-import java.util.Scanner;
+public class IlligalTriangleException extends Exception {
+    public IlligalTriangleException() {
 
-public class IlligalTriangleException {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        // Tam giác có 3 cạnh: x, y, z
-        System.out.println("Enter x: ");
-        int x = scanner.nextInt();
-        System.out.println("Enter y: ");
-        int y = scanner.nextInt();
-        System.out.println("Enter z: ");
-        int z = scanner.nextInt();
-
-        IlligalTriangleException illigalTriangleException = new IlligalTriangleException();
-        illigalTriangleException.check(x, y, z);
-    }
-
-    private void check(int x, int y, int z) throws Exception {
-        try {
-            if (x < 0 || y < 0 || z < 0) {
-                throw new Exception();
-            } else {
-                if (x + y < z || x + z < y || y + z < x) {
-                    throw new Exception();
-                } else {
-                    System.out.println("Triangle is oke");
-                }
-            }
-        } catch (Exception e) {
-            System.err.println("try it again boy");
-        }
     }
 }
---
+++ src/MyLop.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MyLop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Tam giác có 3 cạnh: x, y, z
+        System.out.println("Enter x: ");
+        int x = scanner.nextInt();
+        System.out.println("Enter y: ");
+        int y = scanner.nextInt();
+        System.out.println("Enter z: ");
+        int z = scanner.nextInt();
+
+        check(x, y, z);
+    }
+
+    private static void check(int x, int y, int z) {
+        try {
+            if (x < 0 || y < 0 || z < 0) {
+                throw new IlligalTriangleException();
+            } else {
+                if (x + y < z || x + z < y || y + z < x) {
+                    throw new IlligalTriangleException();
+                } else {
+                    System.out.println("Triangle is oke");
+                }
+            }
+        } catch (IlligalTriangleException e) {
+            System.err.println("try it again boy");
+        }
+    }
+}
